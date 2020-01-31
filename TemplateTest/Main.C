2004f4cf6ef6f70abbd1b741cee01f23d467cdd4
InGyum <kig6416@naver.com>
2020-01-31 11:36:38 +0900

    2장작업

--- TemplateTest/Main.C
+++ TemplateTest/Main.C
@@ -12,7 +12,14 @@ int main() {
 	printf("Goot\bd\tchance\n");
 	printf("Cow\rW\a\n");
 	// type here
-	
+
+	printf("%d\n",10);
+	printf("%lf\n",3.1);
+	printf("%1.lf\n",3.14);
+	printf("%.10lf\n",3.145924426);
+
+	printf("%d과 %d의 합은 %d입니다.\n", 10,20,10+20);
+	printf("%.1lf-%.1lf=%.1lf\n",3.4,1.2,3.4-1.2);
 	system("pause");
 
 	return 0;
