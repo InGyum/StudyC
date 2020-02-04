3359859d07aa5fe8660107e19b7fe620d6ed63c3
InGyum <kig6416@naver.com>
2020-02-04 17:06:00 +0900

    11장 완료

--- CharTestApp/main.c
+++ CharTestApp/main.c
@@ -1,19 +1,32 @@
 /*
   filename - main.c
   version - 1.0
-  description - 기본 메인 함수
+  description - 기본 메인 함수 /문자 학습
   --------------------------------------------------------------------------------
-  first created - 2020.02.01
-  writer - Hugo MG Sung.
+  first created - 2020.02.04
+  writer - 김인겸.
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+
 // 메인함수
+
+
 int main(void) 
 {
-	printf("Hello World!\n");
+    int num, grade;
+
+    printf("학번 입력 : ");
+    scanf("%d", &num);
+    getchar();
+    printf("학점 입력 : ");
+    grade = getchar();
+    printf("학번 : %d, 학점 : %c\n", num, grade);
+
+
+
     // type here.
 	system("pause");
 	return EXIT_SUCCESS;
