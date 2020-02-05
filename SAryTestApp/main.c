a6770406ea8b4d44149686621e9e939aa3961e06
InGyum <kig6416@naver.com>
2020-02-05 17:18:24 +0900

    14장

---
+++ SAryTestApp/main.c
@@ -0,0 +1,37 @@
+/*
+  filename - main.c
+  version - 1.0
+  description - 기본 메인 함수
+  --------------------------------------------------------------------------------
+  first created - 2020.02.05
+  writer - Hugo MG Sung.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// 메인함수
+int main(void) 
+{
+
+    char animal[5][20];
+
+    int i;
+    int count;
+
+    count = sizeof(animal) / sizeof(animal[0]);
+    for (i = 0; i < count; i++)
+    {
+        scanf("%s", animal[i]);
+    }
+    for (i = 0; i < count; i++)
+    {
+        printf("%s", animal[i]);
+    }
+
+
+
+    // type here.
+	system("pause");
+	return EXIT_SUCCESS;
+}
